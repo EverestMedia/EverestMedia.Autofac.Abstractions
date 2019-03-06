a4e5b9f0ce874c9c5fadcea413d13148440e782d
Liam Prowse <itsliamprowse@gmail.com>
2019-03-06 11:43:11 +0000

    brought in the non core project

---
+++ EverestMedia.Autofac.Abstractions/EverestMedia.Autofac.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EverestMedia.Autofac.Abstractions")]
+[assembly: AssemblyDescription("Autofac helper abstractions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Everest Media ltd")]
+[assembly: AssemblyProduct("EverestMedia.Autofac.Abstractions")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bae1d8a1-aa08-438b-be72-9ecd15660fd9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
